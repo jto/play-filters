b5cf7e615445d7650d64da8b63e5564500afc403
jtournay <jtournay@linkedin.com>
2012-05-16 05:55:16 -0700

    typo

--- app/Filters.scala
+++ app/Filters.scala
@@ -1,4 +1,4 @@
-// Copyright 2012 Julien Tourna
+// Copyright 2012 Julien Tournay
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
