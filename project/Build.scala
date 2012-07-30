bedf78c757408cef15c5c5af6f6942c2f7c6734e
jtournay <jtournay@linkedin.com>
2012-07-30 14:27:25 +0200

    set maven style to false

--- project/Build.scala
+++ project/Build.scala
@@ -25,7 +25,6 @@ object ApplicationBuild extends Build {
       val artifactory = "http://artifactory.corp.linkedin.com:8081/artifactory/"
       val mavenLocal = Resolver.file("file",  new File(Path.userHome.absolutePath + "/Documents/mvn-repo/snapshots"))
       val sandbox = Resolver.url("Artifactory sandbox", url(artifactory + "ext-sandbox"))(Patterns("[organisation]/[module]/[revision]/[artifact]-[revision].[ext]"))
-
     }
 
     val appDependencies = Seq(
@@ -36,7 +35,8 @@ object ApplicationBuild extends Build {
       organization := "jto",
       resolvers += Repos.sandbox,
       publishTo := Some(Repos.sandbox),
-      credentials += Credentials(Path.userHome / ".sbt" / ".licredentials")
+      credentials += Credentials(Path.userHome / ".sbt" / ".licredentials"),
+      publishMavenStyle :=  false
     )
 
 }
